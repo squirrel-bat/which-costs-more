e712f380cae57ea84dc7d880af3d19eba8729eea
squirrel-bat <8386773+squirrel-bat@users.noreply.github.com>
2023-10-25 14:29:25 +0200

    Refactor parse_data function to use generator expression

--- import/importer.py
+++ import/importer.py
@@ -15,29 +15,30 @@ def get_bulk_json():
     return requests.get(res["download_uri"]).json()
 
 
+def is_valid_card(card):
+    return (
+        card["prices"]["usd"] is not None
+        and card["prices"]["eur"] is not None
+        and "image_uris" in card
+        and "normal" in card["image_uris"]
+        and card["lang"] == "en"
+        and card["digital"] is False
+        and "paper" in card["games"]
+    )
+
+
 def parse_data(data_json):
     print(f"Success.{HR}Read:    {len(data_json)}")
-    card_data = []
-    for card in data_json:
-        if (
-            None in (card["prices"]["eur"], card["prices"]["usd"])
-            or "image_uris" not in card
-            or "normal" not in card["image_uris"]
-            or card["lang"] != "en"
-            or card["digital"] is True
-            or "paper" not in card["games"]
-        ):
-            continue
-        else:
-            card_stripped = {
-                "name": card["name"],
-                "img_uri": card["image_uris"]["normal"],
-                "prices": card["prices"],
-                "scryfall_uri": card["scryfall_uri"],
-            }
-            card_data.append(card_stripped)
-    if len(card_data) == 0:
-        return
+    card_data = list(
+        {
+            "name": card["name"],
+            "img_uri": card["image_uris"]["normal"],
+            "prices": card["prices"],
+            "scryfall_uri": card["scryfall_uri"],
+        }
+        for card in data_json
+        if is_valid_card(card)
+    )
     json_data = json.dumps(card_data)
     with gzip.open(OUTPUT_FILE, "w") as fp:
         fp.write(json_data.encode("utf-8"))
